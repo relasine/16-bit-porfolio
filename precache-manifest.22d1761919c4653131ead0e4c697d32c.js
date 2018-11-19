910517fca13a679b37c009dcf919f8a0d6e1a3fc
Kevin Simpson <relasine@gmail.com>
2018-11-18 19:39:43 -0700

    Updates

---
+++ precache-manifest.22d1761919c4653131ead0e4c697d32c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c15f4e88d9bba13f08898f98ee671edf",
+    "url": "/16-bit-porfolio/static/media/tavern.c15f4e88.png"
+  },
+  {
+    "revision": "5426e8f5b0dd56c794ff0aa9cc1025f3",
+    "url": "/16-bit-porfolio/static/media/itemshop.5426e8f5.png"
+  },
+  {
+    "revision": "d66d52edf9a1c1c07ecae68a7ab54462",
+    "url": "/16-bit-porfolio/static/media/house.d66d52ed.png"
+  },
+  {
+    "revision": "9b569dca8615e799ec233460092ae8f7",
+    "url": "/16-bit-porfolio/static/media/ChronoType.9b569dca.ttf"
+  },
+  {
+    "revision": "2c2ef2b72530299d9180",
+    "url": "/16-bit-porfolio/static/js/runtime~main.2c2ef2b7.js"
+  },
+  {
+    "revision": "91b1406178ec195a374b",
+    "url": "/16-bit-porfolio/static/js/main.91b14061.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/16-bit-porfolio/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "91b1406178ec195a374b",
+    "url": "/16-bit-porfolio/static/css/main.1aedd47a.chunk.css"
+  },
+  {
+    "revision": "b75384c5825c28d7b71ce32aabfccfba",
+    "url": "/16-bit-porfolio/index.html"
+  }
+];
